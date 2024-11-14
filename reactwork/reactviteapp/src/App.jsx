ee953d5c9d82bed8ef648125f1eade4622a70de6
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-11-14 13:01:04 +0530

    add work for array mapping

--- reactwork/reactviteapp/src/App.jsx
+++ reactwork/reactviteapp/src/App.jsx
@@ -1,45 +1,70 @@
-import React from 'react'
-import Student from './Student';
+import React from "react";
+import Student from "./Student";
 function App() {
-  const h1=<h1>Hello World</h1>;
- const mystyle={
-  color:'red',
-  backgroundColor:'yellow'
- }
-  return (
-    <div style={{backgroundColor:'cyan'}}>
-      {h1}
-    
-    <div style={mystyle}>
-      ABES Engineering College
-    </div>
-    <div>
-      <Student college="ABES Engineering College" 
-      name="Rahul"
-      branch="CSE"
-      roll="2328392"
-      section="A"
-      
-      />
+  const h1 = <h1>Hello World</h1>;
+  const mystyle = {
+    color: "red",
+    backgroundColor: "yellow",
+    display: "flex",
+  };
 
-<Student college="ABES Engineering College" 
-      name="Ankit"
-      pic={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100}/>}
-      branch="CSE"
-      roll="787878"
-      section="A"
-      
-      />
-      <Student college="ABES Engineering College" 
-      name="Amit Tomer"
-      branch="CSE"
-      roll="6754890"
-      section="A"
-      
-      />
+  const studData = [
+    {
+      college: "ABES Engineering College",
+      name: "Ankit",
+      pic: (
+        <img
+          src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
+          height={100}
+          width={100}
+        />
+      ),
+      branch: "CSE",
+      roll: "787878",
+      section: "B",
+    },
+    {
+      college: "ABES Engineering College",
+      name: "Ankit",
+      pic: (
+        <img
+          src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
+          height={100}
+          width={100}
+        />
+      ),
+      branch: "CSE",
+      roll: "787878",
+      section: "B",
+    },
+    {
+      college: "ABES Engineering College",
+      name: "Ankit",
+      pic: (
+        <img
+          src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
+          height={100}
+          width={100}
+        />
+      ),
+      branch: "CSE",
+      roll: "787878",
+      section: "B",
+    },
+  ];
+
+  return (
+    <div style={{ backgroundColor: "cyan" }}>
+      {h1}
+
+      <div style={mystyle}>ABES Engineering College</div>
+      <div style={{ display: "flex" }}>
+        {studData.map((item, index) => (
+          <Student key={index} props={item} />
+        ))}
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
