acd2126edcbb86a3d1569f76a99099fbdc09ffe1
Rudraksh Tyagi <59254790+dragon-slayer875@users.noreply.github.com>
2024-11-21 11:49:32 +0530

    minor changes

--- reactwork/reactviteapp/src/App.jsx
+++ reactwork/reactviteapp/src/App.jsx
@@ -5,63 +5,42 @@ function App() {
   const mystyle = {
     color: "red",
     backgroundColor: "yellow",
-    display: "flex",
   };
-
-  const studData = [
-    {
-      college: "ABES Engineering College",
-      name: "Ankit",
-      pic: (
-        <img
-          src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
-          height={100}
-          width={100}
-        />
-      ),
-      branch: "CSE",
-      roll: "787878",
-      section: "B",
-    },
-    {
-      college: "ABES Engineering College",
-      name: "Ankit",
-      pic: (
-        <img
-          src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
-          height={100}
-          width={100}
-        />
-      ),
-      branch: "CSE",
-      roll: "787878",
-      section: "B",
-    },
-    {
-      college: "ABES Engineering College",
-      name: "Ankit",
-      pic: (
-        <img
-          src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
-          height={100}
-          width={100}
-        />
-      ),
-      branch: "CSE",
-      roll: "787878",
-      section: "B",
-    },
-  ];
-
   return (
     <div style={{ backgroundColor: "cyan" }}>
       {h1}
 
       <div style={mystyle}>ABES Engineering College</div>
-      <div style={{ display: "flex" }}>
-        {studData.map((item, index) => (
-          <Student key={index} props={item} />
-        ))}
+      <div>
+        <Student
+          college="ABES Engineering College"
+          name="Rahul"
+          branch="CSE"
+          roll="2328392"
+          section="A"
+        />
+
+        <Student
+          college="ABES Engineering College"
+          name="Ankit"
+          pic={
+            <img
+              src="https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png"
+              height={100}
+              width={100}
+            />
+          }
+          branch="CSE"
+          roll="787878"
+          section="A"
+        />
+        <Student
+          college="ABES Engineering College"
+          name="Amit Tomer"
+          branch="CSE"
+          roll="6754890"
+          section="A"
+        />
       </div>
     </div>
   );
